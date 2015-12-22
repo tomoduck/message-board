537223653eabc88ec0fc239c75cefcbfc6c71fa7
tomosuke <tomo.yoropiko@hotmail.co.jp>
2015-12-22 11:58:54 +0000

    age

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -41,7 +41,7 @@ class MessagesController < ApplicationController
   end
   private
   def message_params
-    params.require(:message).permit(:name, :body)
+    params.require(:message).permit(:name, :body, :age)
   end
   def set_message
     @message = Message.find(params[:id])
